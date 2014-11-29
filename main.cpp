8d0a139ef8ae197e4ee73988e31d104a294071b6
Michael Ho <callmemikeh@gmail.com>
2014-11-28 23:04:49 -0800

    xbee sending almost works
    however, still sending some garbage values when not sending. Data sends and is received correctly, just there is extra garbage being printed.
    This may have something to do with the board I'm using right now, which is the USB explorer, i read something about it being a problem because the RSSI pin is connected to another pull-up resistor or what not. We'll see what happens when we switch back to the mbed standard breakout board.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include "mbed.h"
 #include <limits.h>
 #include <stdio.h>
-Serial device(p9, p10); //tx, rx
-DigitalOut xbeeout(p11);
+#include "Servo.h"
+#include "xbee.h"
+
+Serial spoke_sensor(p9, p10); //tx, rx
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 DigitalOut led3(LED3);
 DigitalOut led4(LED4);
 Serial pc(USBTX, USBRX); // tx, rx
-Serial xbee(p13, p14);
+xbee xbee(p13, p14, p12);
+Servo servo(p21);
 
 void wheelspeed_interrupt();
 volatile int count = 0; //wheelspeed_interrupt increments this, reset to 0 after send_interval
@@ -17,15 +20,25 @@ int num_spokes = 24; //set to number of spokes on wheel, used for calculating sp
 int send_interval = 2; //interval between sending data via XBEE, in seconds
 double circumference = 0.00130239; // in miles for 700x23c wheel w/ tire
 int update_interval = 2000; // in ms
+double speed_per_spoke;
+float pos;
+float new_pos = 0.0;
+char speed_buffer[50];
+char *speed_buffer_val;
+double shift_vals[11];
 
 /* Prints speed to terminal through a usb. */
-void show_usbterm_speed(double speed) {
+void show_usbterm_speed(double speed, float pos) {
 	pc.printf("speed: %f\n", speed);
+	pc.printf("servo position: %f\n", pos);
 }
 
 /* Sends speed to XBEE. */
 void send_xbee_speed(double speed) {
-	xbee.printf("speed: %f\n", speed);
+	sprintf(speed_buffer_val, "%f", speed);
+	xbee.SendData(speed_buffer);
+	//xbee.printf("speed: %f\n", speed);
+	//pc.printf("XBEE SEND\n");
 }
 
 /* Shows the speed using the 4 leds as binary values.
@@ -55,15 +68,36 @@ void show_binary_speed(double speed) {
 	}
 }
 
+void telemetry_init() {
+	// convert units before to minimize calculations in critical section
+	pc.printf("Initializing.\n");
+	speed_per_spoke = circumference * 3600 * 1000 / update_interval / num_spokes;
+	spoke_sensor.attach(&wheelspeed_interrupt, Serial::RxIrq);
+	sprintf(speed_buffer, "speed: ");
+	speed_buffer_val = speed_buffer + 7;
+}
+
+void shift_init() {
+	//shift_vals = {0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+	servo.calibrate(0.001, 0.0);
+}
+void init() {
+	spoke_sensor.baud(9600);
+	telemetry_init();
+	shift_init();
+}
 /* Main sending loop. */
 int main() {
-	// convert units before to minimize calculations in critical section
-	double speed_per_spoke = circumference * 3600 * 1000 / update_interval / num_spokes;
+	init();
 	int send_time_left = send_interval;
-	device.baud(9600);
-	device.attach(&wheelspeed_interrupt, Serial::RxIrq);
-	pc.printf("Starting logging.\n");
+	pc.printf("Starting Logging.\n");
 	while(1) {
+		pos = servo.read();
+		servo.write(new_pos);
+		new_pos = new_pos + 0.5;
+		if (new_pos > 1.0) {
+			new_pos = 0.0;
+		}
 		wait_ms(update_interval / 2);
 		led1 = 1;
 		wait_ms(update_interval / 2);
@@ -74,22 +108,26 @@ int main() {
 		NVIC_EnableIRQ(UART1_IRQn); //end critical section
 		led1 = 0;
 		led2 = 0;
-		show_usbterm_speed(speed);
+		show_usbterm_speed(speed, pos);
 		send_time_left -= 1;
-		if (send_time_left == 0) {
+		//if (send_time_left == 0) {
 			send_xbee_speed(speed);
-			send_time_left = send_interval;
-		}
+			//send_time_left = send_interval;
+		//}
 	}
 }
 
 /* Serial interrupt for light sensor
  * measures number of times light is interrupted by spoke to calculate wheelspeed.
  */
-
 void wheelspeed_interrupt() {
 	led2 = 1;
-	device.getc();
+	spoke_sensor.getc();
 	count ++;
 	return;
 }
+
+/* Serial interrupt for shift up. */
+void shift_up() {
+	led2 = 1;
+}	
